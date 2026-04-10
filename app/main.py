cd5c0faa1db4f7800485789bdc19bd03c159345a
Fetivol <Fetisowova@gmail.com>
2026-04-10 20:41:29 +0200

    created class Distance with methods, used 1 static method and future @total_ordering from guidelines

--- app/main.py
+++ app/main.py
@@ -1,3 +1,39 @@
+from __future__ import annotations
+from functools import total_ordering
+
+
+@total_ordering
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    @staticmethod
+    def get_km(other: Distance | int | float) -> int | float:
+        return other.km if isinstance(other, Distance) else other
+
+    def __add__(self, other: Distance | int) -> Distance:
+        return Distance(
+            self.km + self.get_km(other)
+        )
+
+    def __iadd__(self, other: Distance | int) -> Distance:
+        self.km += self.get_km(other)
+        return self
+
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int | float) -> Distance:
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: Distance | int | float) -> bool:
+        return self.km < self.get_km(other)
+
+    def __eq__(self, other: Distance | int | float) -> bool:
+        return self.km == self.get_km(other)
